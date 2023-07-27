18e002f036798e2cffcf7a989c19835528209e89
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-07-27 23:19:01 +0330

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+
 const App = () => {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
+
   const counterHandler = (op) => {
+    if (op === "DEC" && counter === 0) {
+      return (null)
+    }
     dispatch({ type: op });
   };
+
   return (
     <div>
       <h1>Counter App</h1>
-      <h2>{counter}</h2>
-      <button onClick={() => counterHandler("INC")}>increment</button>
-      <button onClick={() => counterHandler("DEC")}>decrement</button>
+      <h1>{counter}</h1>
+      <button onClick={() => counterHandler("INC")}>Increament</button>
+      <button onClick={() => counterHandler("DEC")}>Decreament</button>
     </div>
   );
 };
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,16 +1,31 @@
+// import { legacy_createStore as createStore } from "redux";
+
+// const reducerFunction = (state = { counter: 0 }, action) => {
+//   if (action.type === "INC") {
+//     return { counter: state.counter + 1 };
+//   }
+//   if (action.type === "DEC") {
+//     return { counter: state.counter - 1 };
+//   }
+//   return state;
+// };
+
+// const store = createStore(reducerFunction);
+
+// export default store;
+
 import { legacy_createStore as createStore } from "redux";
 
-const reducerFunctoin = (state = { counter: 0 }, action) => {
+const reducerFunction = (state = { counter: 0 }, action) => {
   if (action.type === "INC") {
     return { counter: state.counter + 1 };
   }
   if (action.type === "DEC") {
     return { counter: state.counter - 1 };
   }
-
   return state;
 };
 
-const store = createStore(reducerFunctoin);
+const store = createStore(reducerFunction);
 
 export default store;
