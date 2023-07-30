f05f7601e9760960f339f07d885cb282ed4a62c2
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-07-30 13:40:56 +0330

    using configureStore and createSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,28 @@
-// import React from "react";
-// import "./App.css";
-// import { useDispatch, useSelector } from "react-redux";
-// import { actions } from "./store/index";
-
-// const App = () => {
-//   const counter = useSelector((state) => state.counter);
-//   const dispatch = useDispatch();
-
-//   const counterHandler = (op) => {
-//     dispatch(op);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Counter App</h1>
-//       <h1>{counter}</h1>
-//       <button onClick={() => counterHandler(actions.increment())}>Increase</button>
-//       <button onClick={() => counterHandler(actions.decrement())}>Decrease</button>
-//       <button onClick={() => counterHandler(actions.addBy(60))}>Add by 10</button>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { actions } from "./store";
 
 const App = () => {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
-
   const counterHandler = (op) => {
-    if (op === "ADD") {
-      dispatch({ type: op, payload: 10 });
-    } else if (op === "DEC" && counter === 0) {
-      null;
-    } else {
-      dispatch({ type: op });
-    }
+    dispatch(op);
   };
 
   return (
     <div>
       <h1>Counter App</h1>
       <h1>{counter}</h1>
-      <button onClick={() => counterHandler("INC")}>increment</button>
-      <button onClick={() => counterHandler("DEC")}>decrement</button>
-      <button onClick={() => counterHandler("ADD")}>add by 10</button>
+      <button onClick={() => counterHandler(actions.increment())}>
+        increment
+      </button>
+      <button onClick={() => counterHandler(actions.decrement())}>
+        decrement
+      </button>
+      <button onClick={() => counterHandler(actions.addBy(10))}>
+        add by 10
+      </button>
     </div>
   );
 };
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,43 +1,25 @@
-// import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// const counterSlice = createSlice({
-//   name: "counter",
-//   initialState: { counter: 0 },
-//   reducers: {
-//     increment(state, action) {
-//       state.counter ++;
-//     },
-//     decrement(state, action) {
-//       state.counter --;
-//     },
-//     addBy(state, action) {
-//       state.counter += action.payload;
-//     },
-//   },
-// });
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state, action) {
+      state.counter++;
+    },
+    decrement(state, action) {
+      state.counter--;
+    },
+    addBy(state, action) {
+      state.counter += action.payload;
+    },
+  },
+});
 
-// export const actions = counterSlice.actions
+export const actions = counterSlice.actions;
 
-// const store = configureStore({
-//   reducer : counterSlice.reducer
-// })
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-// export default store
-
-import { legacy_createStore as createStore } from "redux";
-
-const reducerFunction = (state = { counter: 0 }, action) => {
-  if(action.type === 'INC'){
-    return {counter : state.counter + 1}
-  }
-  if(action.type === 'DEC'){
-    return {counter : state.counter - 1}
-  }
-  if(action.type === 'ADD'){
-    return {counter : state.counter + action.payload}
-  }
-  return state;
-};
-
-const store = createStore(reducerFunction);
 export default store;
